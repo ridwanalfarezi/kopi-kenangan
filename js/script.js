f9c6c09d05c6ed5da2f3689d53f1116a43646f13
ridwan <alfareziridwan@gmail.com>
2023-03-19 16:00:00 +0700

    Remove URL Anchor ID

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 // Toggle Hamburger Menu
-const nav = document.querySelector('.nav-link');
-const hamburgerMenu = document.querySelector('#hamburger-menu')
+const nav = document.querySelector(".nav-link");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
 
-hamburgerMenu.addEventListener('click', () => {
-    nav.classList.toggle('active');
-})
+hamburgerMenu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
 
 // Hilangkan Nav dimanapun layar di klik
-document.addEventListener('click', (e) => {
-    if(!hamburgerMenu.contains(e.target) && !nav.contains(e.target)) {
-        nav.classList.remove('active');
-    }
-})
+document.addEventListener("click", (e) => {
+  if (!hamburgerMenu.contains(e.target) && !nav.contains(e.target)) {
+    nav.classList.remove("active");
+  }
+});
 
 // Transparent Nav BG
 window.onscroll = () => {
-    const navbar = document.querySelector('.navbar')
-    const fixedNav = navbar.offsetTop;
+  const navbar = document.querySelector(".navbar");
+  const fixedNav = navbar.offsetTop;
 
-    if (window.pageYOffset > fixedNav ) {
-        navbar.classList.add('navbar-fixed');        
-    } else {
-        navbar.classList.remove('navbar-fixed');        
-    }
-}
+  if (window.pageYOffset > fixedNav) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+};
+
+// Remove Anchor ID
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
